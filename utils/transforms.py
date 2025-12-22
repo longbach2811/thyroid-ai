0538881519535409fdc82c4ba2ea23bb781c5901
政儒 陳 <D0703202@cgu.edu.tw>
2025-12-22 17:53:40 +0800

    Add new transforms for better learning

--- utils/transforms.py
+++ utils/transforms.py
@@ -7,26 +7,21 @@ import cv2
 def _get_train_transforms(imgsz=512):
     train_aug = A.Compose([
         A.LongestMaxSize(max_size=imgsz),
-        A.PadIfNeeded(
-            min_height=imgsz,
-            min_width=imgsz,
-            border_mode=cv2.BORDER_CONSTANT,
-            fill=0            
-        ),
+        A.PadIfNeeded(min_height=imgsz, min_width=imgsz, border_mode=cv2.BORDER_CONSTANT, fill=0),
 
+        # Geometric
+        A.RandomRotate90(p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=20, p=0.5),
         A.HorizontalFlip(p=0.5),
-        A.ColorJitter(
-            brightness=0.2,
-            contrast=0.2,
-            saturation=0.2,
-            hue=0.05,
-            p=0.5
-        ),
+        A.VerticalFlip(p=0.3),
+        
+        # Photometric
+        A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05, p=0.5),
         A.GaussianBlur(blur_limit=3, p=0.2),
-        A.Normalize(
-            mean=(0.485, 0.456, 0.406),
-            std=(0.229, 0.224, 0.225)
-        ),
+        A.GaussNoise(var_limit=(10.0, 50.0), p=0.2),
+
+        # Normalize
+        A.Normalize(mean=(0.485,0.456,0.406), std=(0.229,0.224,0.225)),
         ToTensorV2()
     ])
     return train_aug
